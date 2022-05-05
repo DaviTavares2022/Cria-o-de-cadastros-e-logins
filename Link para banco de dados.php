a39716186599371d7c75dd324f83e959b86b4cf2
DaviTavares2022 <104947066+DaviTavares2022@users.noreply.github.com>
2022-05-04 21:28:38 -0300

    Add files via upload

---
+++ Link para banco de dados.php
@@ -0,0 +1,18 @@
+<?php
+ $dbhost="localhost";
+ $dbUsername = 'root';
+ $dbPassword = '';
+ $dbname = 'cadstro Hublocal';
+
+ $conexao = new mysqli($dbhost,$dbUsername,$dbPassword,$dbname);
+
+//if($conexao->connect_errno)
+//{
+//  echo"Erro";
+// }
+// else
+//{
+//    echo"Cadastro feito com sucesso";
+//}
+
+ ?>
